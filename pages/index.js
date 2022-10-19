8197bcd71bc5db3f3346e9f885885a329ed13a97
murkhunting <marcmp.arq@gmail.com>
2022-10-19 13:15:01 +0200

    create layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Layout from "../layouts/Layout";
+
 export default function Home() {
   return (
-    <div className="home">
-      <h1>HOME</h1>
-    </div>
+    <Layout>
+      <div className="home">
+        <h1>HOME</h1>
+      </div>
+    </Layout>
   );
 }
