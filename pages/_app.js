1593d93bbd39a43fe544c2ae3ea83e7fd7303e17
murkhunting <marcmp.arq@gmail.com>
2022-11-22 21:03:00 +0100

    simple change

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import Footer from "../components/footer/Footer";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar {...initialLang} />
+      <Navbar />
       <Component {...pageProps} />
       <Footer />
     </>
