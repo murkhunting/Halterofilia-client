67260ec252e83dc78a6911f3acc270f66796ab5b
murkhunting <marcmp.arq@gmail.com>
2022-11-22 21:01:26 +0100

    props

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "../scss/global.scss";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 
-function MyApp({ Component, pageProps, initialLang }) {
+function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar {...initialLang} />
